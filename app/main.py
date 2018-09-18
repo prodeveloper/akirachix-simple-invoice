1a419e420c732b7212cb9487bfec3a496b05de5c
Jacob Chencha <jmokua@gmail.coma>
2018-09-18 11:33:09 +0300

    Able to create a new invoice from a form

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, request)
 from models.user import User
 from models.invoice import Invoice
 import bootstrap
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return "success"
     bootstrap.initialize()
     user = User.select().where(
         User.email == "john@doe.com"
@@ -20,6 +21,21 @@ def index():
     total = invoice.design_fee + invoice.hosting_fee + invoice.domain_fee
     return render_template('invoice.html', user=user, invoice=invoice, total=total)
 
+
+@app.route('/invoice/generate', methods=['POST', 'GET'])
+def generate():
+    invoice = dict(request.form.items())
+    total = (int(invoice.get('design_fee')) +
+             int(invoice.get('hosting_fee')) +
+             int(invoice.get('domain_fee'))
+             )
+    return render_template('user_generated_invoice.html',
+                           total=total, invoice=invoice)
+
+
+@app.route('/invoice/new')
+def new_invoice():
+    return render_template('invoice_form.html')
 
 if __name__ == '__main__':
     app.run(**app_start_config)
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker container run -p 8080:8080 --rm  -v /Users/jchencha/Google\ Drive/GDocuments/akirachix/classes/sem3/simple_invoice/app:/app akirachix-simple-invoice
+docker container run -p 8080:8080 --rm  -v $(pwd)/app:/app akirachix-simple-invoice
